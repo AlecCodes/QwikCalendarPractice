bf3e56bc95894e9bcfd80d417aefeca9cb46715e
Alec Hannaford <alec322@gmail.com>
2023-11-22 17:29:02 -0500

    new containers for events

--- src/routes/Calendar/index.tsx
+++ src/routes/Calendar/index.tsx
@@ -4,12 +4,29 @@ export default component$(() => {
     return (
     <>
         <section class = 'section bright'> Event Calendar!</section>
-        <div class = 'calendar'>
-            <div class = 'day'>
-                <div class = 'date'>Wed 22nd</div>
-                <div class = 'event'>
-                    <span class = 'eventDescription'> Eat Pizza </span>
+        <div class='calendarContainer'>
+            <div class = 'calendar'>
+                <div class = 'day'>
+                    <div class = 'date'>Wed 22nd</div>
+                    <div class ='eventContainer'>
+                        <div class = 'event'>
+                            <span class = 'eventDescription'> Eat Pizza </span>
+                        </div>
+                    </div>
                 </div>
+
+                <div class='day'>        
+                        <div class = 'date'>Wed 23rd</div>
+                        <div class = 'eventContainer'>
+                            <div class = 'event'>
+                                <span class = 'eventDescription'>Thanksgiving</span>
+                            </div>
+                            <div class = 'event'>
+                                <span class = 'eventDescription'>Nap</span>
+                            </div>
+                        </div>
+                </div>
+
             </div>
         </div>
     </>
