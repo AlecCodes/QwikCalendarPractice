8ae5663c0e6cf1092f88cc7c07a010886ffeadc5
Alec Hannaford <alec322@gmail.com>
2023-11-22 21:12:36 -0500

    event container aligned to end of container, on the right

--- src/routes/Calendar/index.tsx
+++ src/routes/Calendar/index.tsx
@@ -3,9 +3,11 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
     return (
     <>
-        <section class = 'section bright'> Event Calendar!</section>
         <div class='calendarContainer'>
+        <section class = 'section bright'> Event Calendar!</section>
+        <br></br>
             <div class = 'calendar'>
+                
                 <div class = 'day'>
                     <div class = 'date'>Wed 22nd</div>
                     <div class ='eventContainer'>
