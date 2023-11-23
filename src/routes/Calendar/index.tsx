56d992a7785863e9572aed050e2ddd21f28959fd
Alec Hannaford <alec322@gmail.com>
2023-11-22 21:57:08 -0500

    text doesnt spill over event description

--- src/routes/Calendar/index.tsx
+++ src/routes/Calendar/index.tsx
@@ -12,16 +12,28 @@ export default component$(() => {
                     <div class = 'date'>Wed 22nd</div>
                     <div class ='eventContainer'>
                         <div class = 'event'>
-                            <span class = 'eventDescription'> Eat Pizza </span>
+                            <span class = 'eventDescription'>Eat Pizza </span>
                         </div>
                     </div>
                 </div>
 
                 <div class='day'>        
-                        <div class = 'date'>Thurs 23rd</div>
+                    <div class = 'date'>Thurs 23rd</div>
+                    <div class = 'eventContainer'>
+                        <div class = 'event'>
+                            <div class = 'eventDescription'>Thanksgiving dinner party </div>
+                        </div>
+                        <div class = 'event'>
+                            <span class = 'eventDescription'>Nap</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class='day'>        
+                        <div class = 'date'>Friday 25th</div>
                         <div class = 'eventContainer'>
                             <div class = 'event'>
-                                <span class = 'eventDescription'>Thanksgivinggggg</span>
+                                <div class = 'eventDescription'>Flight<span>8AM</span></div>
                             </div>
                             <div class = 'event'>
                                 <span class = 'eventDescription'>Nap</span>
